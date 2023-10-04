4e41110de1a74996dc33891297162efedd326b3e
tejaswpratap7355 <134800837+tejaswpratap7355@users.noreply.github.com>
2023-10-04 04:25:01 +0000

    nums_and_maths

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,35 @@
+const score = 500
+console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 123.7643
+
+// console.log(otherNumber.toPrecision(4));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+// ********************** Mathes ****************
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.1));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(6, 3, 4, 2));
+// console.log(Math.max(6, 3, 4, 2));
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10;
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
