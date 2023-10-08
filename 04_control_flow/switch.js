fc52ae6feec5161af3610a3fa87e9e959d2f5ec4
tejaswpratap7355 <134800837+tejaswpratap7355@users.noreply.github.com>
2023-10-08 05:02:10 +0000

    control flow in js

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,32 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "March"
+
+switch (month) {
+    case "Jan":
+        console.log("Jan");
+        break;
+    case "Feb":
+        console.log("Feb");
+        break;
+    case "March":
+        console.log("March");
+        break;
+    case "April":
+        console.log("April");
+        break;
+    case "May":
+        console.log("May");
+        break;
+    
+    default:
+        console.log("Default case match");
+        break;
+}
